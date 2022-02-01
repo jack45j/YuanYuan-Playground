0ad502a8d1b99f7134fd2e0ee8d862c4aab7476c
Benson Lin <jack45j@gmail.com>
2022-02-01 19:28:38 +0800

    資料結構 Part1

---
+++ YuanYuan.playground/Contents.swift
@@ -0,0 +1,52 @@
+/*
+    - var 變數 variable
+    - let 常數
+ Basic
+    - Int 整數 integer
+    - Float 浮點數 / Double 倍精度
+    - String 字串 / Character 字元 (print)
+    - Bool Boolean 布林 對or錯
+    - Array / Set
+    - Dictionary
+ Bonus
+    - Queue
+    - Stack
+ Bonus knowledge
+    - 0x 16進制
+    - 0b  8進制
+ */
+
+var 名字 = "小明"
+let 物種 = "人"
+var 幾個人: Int = 1
+var 身高: Int = 168
+var 身高公尺: Float = 1.68
+var 剩下: Double = 0.8
+var 是雞掰人: Bool = false
+
+print("他的名字叫\(名字) 他是\(幾個人)個人 他身高\(身高) 也就是\(身高公尺)公尺 他被車撞剩下\(剩下)個\(名字)")
+print("他是雞掰人嗎？ \(是雞掰人)")
+
+名字 = "人格分裂的小美"
+是雞掰人 = true
+
+print("他後來改名叫做\(名字) 他現在很雞掰嗎？ \(是雞掰人)")
+
+名字 = "冠佑"
+是雞掰人 = false
+
+print("他後來又改了名字叫做\(名字) 他現在很雞掰嗎？ \(是雞掰人)")
+
+
+
+
+
+
+var account: String = ""
+var password: String = ""
+var username: String = ""
+
+account = "mag345172@gmail.com"
+password = "J8211jj8211jj"
+username = "RachelChou"
+// 後面就可以存起來或是幫你創帳號
